af6195c4f603174ae60ab9a78764e535d15496c9
michalkuszak <michalkuszak@gmail.com>
2025-05-12 14:56:04 +0200

    Add comments for clarity

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import json
 with open('./morse.json', mode='r') as file:
     MORSE_DICT = json.load(file)
 
+# Endless loop for user inputs
 while True:
     string = input("Please input a string. If you want to quit, type 'q': ").lower()
 
-    if string != 'q':
+# Main program functionality
+    if string != 'q':    # Typing 'q' (case-insensitive) quits the program
         for char in string:
             if char in MORSE_DICT:
                 string = string.replace(char, f"{MORSE_DICT[char]} ")
